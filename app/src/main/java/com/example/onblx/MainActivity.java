28b80e97c63969123db6cbee2f297bfa4bfb6156
NTLong050801 <90180848+NTLong050801@users.noreply.github.com>
2022-04-04 09:11:59 +0700

    update

--- app/src/main/java/com/example/onblx/MainActivity.java
+++ app/src/main/java/com/example/onblx/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.onblx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DataBase db = new DataBase(this,"OnThiBLX.sqlite",null,1);
+
+        db.QueryData("Create table if not exists LoaiCauHoi(IDLoaiCauHoi Integer primary key autoincrement,TenLoaiCauHoi varchar)");
+
+        //thêm dữ liệu
+        db.QueryData("INSERT INTO LoaiCauHoi Values(null,'Lý thuyết')");
+        //lấy dữ liệu
+        Cursor cursor = db.GetData("SELECT * from LoaiCauHoi");
+        while (cursor.moveToNext()){
+            Toast.makeText(this, cursor.getString(1), Toast.LENGTH_SHORT).show();
+        }
     }
 }
