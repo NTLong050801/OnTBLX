0a212192ea2240a18415f3fc961b649ffea81326
NTLong050801 <90180848+NTLong050801@users.noreply.github.com>
2022-04-04 10:29:17 +0700

    update

--- app/src/main/java/com/example/onblx/MainActivity.java
+++ app/src/main/java/com/example/onblx/MainActivity.java
@@ -18,9 +18,9 @@ public class MainActivity extends AppCompatActivity {
         db.QueryData("Create table if not exists LoaiCauHoi(IDLoaiCauHoi Integer primary key autoincrement,TenLoaiCauHoi varchar)");
 
         //thêm dữ liệu
-        db.QueryData("INSERT INTO LoaiCauHoi Values(null,'Lý thuyết')");
+       // db.QueryData("INSERT INTO LoaiCauHoi Values(null,'Lý thuyết')");
         //lấy dữ liệu
-        Cursor cursor = db.GetData("SELECT * from LoaiCauHoi");
+        Cursor cursor = db.GetData("SELECT * from CauHoi");
         while (cursor.moveToNext()){
             Toast.makeText(this, cursor.getString(1), Toast.LENGTH_SHORT).show();
         }
