6ab4e85f01b7cab3b59897f432131adb626662ca
minh7721 <nhatminh7721@gmail.com>
2022-04-05 01:52:48 +0700

    giao dien list cau hoi

---
+++ app/src/main/java/com/example/onblx/listCauHoi.java
@@ -0,0 +1,14 @@
+package com.example.onblx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listCauHoi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_cau_hoi);
+    }
+}
