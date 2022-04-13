4b257b2b9d9eac5c3bc855536f87c07aee46c708
minh7721 <nhatminh7721@gmail.com>
2022-04-14 00:29:29 +0700

    fix bien bao

--- app/src/main/java/com/example/onblx/Model.java
+++ app/src/main/java/com/example/onblx/Model.java
@@ -44,7 +44,7 @@ public class Model {
         Cursor cursor = sqLiteDatabase.rawQuery(sql, null);
         cursor.moveToFirst();
         while (!cursor.isAfterLast()) {
-            BienBaoModels bienBaoModels = new BienBaoModels(cursor.getString(0), cursor.getString(1), cursor.getString(2), cursor.getBlob(4));
+            BienBaoModels bienBaoModels = new BienBaoModels(cursor.getString(0), cursor.getString(1), cursor.getString(4), cursor.getBlob(3));
             listBienBao.add(bienBaoModels);
             cursor.moveToNext();
         }
@@ -58,7 +58,7 @@ public class Model {
         Cursor cursor = sqLiteDatabase.rawQuery(sql, null);
         cursor.moveToFirst();
         while (!cursor.isAfterLast()) {
-            BienBaoModels bienBaoModels = new BienBaoModels(cursor.getString(0), cursor.getString(1), cursor.getString(3), cursor.getBlob(4));
+            BienBaoModels bienBaoModels = new BienBaoModels(cursor.getString(0), cursor.getString(1), cursor.getString(2), cursor.getBlob(3));
             listBienBao.add(bienBaoModels);
             cursor.moveToNext();
         }
